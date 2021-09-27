c2670cfa69a90ee5c5555310448882316977e02b
Wason <wason_fok@163.com>
2021-09-27 13:18:53 +0800

    Initial version

---
+++ customize/customize-config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+############# Modify default parameters here #################
+WORKDIR=/workdir
+HOSTNAME=GL-MT1300
+IPADDRESS=192.168.8.1
+SSID=MT1300
+ENCRYPTION=psk2+ccmp
+KEY=goodlife
+LEAN_SETTINGS=/workdir/openwrt/package/lean/default-settings/files/zzz-default-settings
+LIENOL_SETTINGS=/workdir/openwrt/package/default-settings/files/zzz-default-settings
+###############################################################
+
+cd "$WORKDIR/openwrt"
+
+# Modify HostName
+sed -i "s/hostname='OpenWrt'/hostname='$HOSTNAME'/g" package/base-files/files/bin/config_generate
+
+# 修改默认 IP
+# 在 21.02 中，不能使用此 sed 命令修改 config generate 中的默认 ipaddress。
+#sed -i 's/192.168.1.1/$IPADDRESS/g' package/base-files/files/bin/config_generate
+
+# 如果该 OpenWrt 是 Lean 或者 Lienol 则修改该默认 ipaddress
+if [ -f "$LIENOL_SETTINGS" ]
+then
+    sed -i -e '/exit 0/d' $LIENOL_SETTINGS
+    echo "uci set network.lan.ipaddr=$IPADDRESS" >> $LIENOL_SETTINGS
+    echo 'uci commit network' >> $LIENOL_SETTINGS
+    echo 'exit 0' >> $LIENOL_SETTINGS
+else
+    if [ -f "$LEAN_SETTINGS" ]
+    then
+        sed -i -e '/exit 0/d' $LEAN_SETTINGS
+        echo "uci set network.lan.ipaddr=$IPADDRESS" >> $LEAN_SETTINGS
+        echo 'uci commit network' >> $LEAN_SETTINGS
+        echo 'exit 0' >> $LEAN_SETTINGS
+    else 
+        sed -i 's/192.168.1.1/$IPADDRESS/g' package/base-files/files/bin/config_generate
+    fi
+fi
+
+# Modify Timezone
+sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a\                set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+
+# Modify NTP settings
+sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/pool.ntp.org/g' package/base-files/files/bin/config_generate
+
+# Modify default WiFi SSID
+sed -i "s/set wireless.default_radio\${devidx}.ssid=OpenWrt/set wireless.default_radio\${devidx}.ssid='$SSID'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default WiFi Encryption
+sed -i "s/set wireless.default_radio\${devidx}.encryption=none/set wireless.default_radio\${devidx}.encryption='$ENCRYPTION'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default WiFi Key
+sed -i "/set wireless.default_radio\${devidx}.mode=ap/a\                        set wireless.default_radio\${devidx}.key='$KEY'" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Forced WiFi to enable
+sed -i 's/set wireless.radio\${devidx}.disabled=1/set wireless.radio\${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
---
+++ customize/customize-feeds-gl.sh
@@ -0,0 +1,67 @@
+cd /workdir
+mkdir -p /workdir/openwrt/package/lean
+
+# Add luci-app-ssr-plus
+cd /workdir/openwrt/package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+
+# Copy 指定插件到 GL-inet OpenWrt 中
+cd /workdir/lede/package/lean
+plist="shadowsocksr-libev pdnsd-alt microsocks dns2socks simple-obfs v2ray-plugin v2ray xray trojan ipt2socks redsocks2 kcptun luci-app-zerotier luci-app-serverchan adbyby luci-app-adbyby-plus"
+for dir in $plist
+do
+    if [ -d $dir ]
+    then
+        echo "Copying plugin $dir to /workdir/openwrt/package/lean ..."
+        cp -rp $dir /workdir/openwrt/package/lean/
+    else
+        echo "$dir does not exists..."
+    fi
+done
+
+cd /workdir/openwrt
+[ -d /workdir/openwrt/feeds/gli_pub/shadowsocks-libev ] && mv /workdir/openwrt/feeds/gli_pub/shadowsocks-libev /workdir/openwrt/feeds/gli_pub/shadowsocks-libev.bak
+if [ -d /workdir/lede/feeds/packages/net/shadowsocks-libev ]
+then
+    [ -d /workdir/openwrt/feeds/packages/net/shadowsocks-libev ] && mv /workdir/openwrt/feeds/packages/net/shadowsocks-libev /workdir/openwrt/feeds/packages/net/shadowsocks-libev.bak
+    [ -d /workdir/openwrt/feeds/gli_pub ] && cp -r /workdir/lede/feeds/packages/net/shadowsocks-libev /workdir/openwrt/feeds/gli_pub/shadowsocks-libev
+    [ -d /workdir/openwrt/package/lean/helloworld ] && cp -r /workdir/lede/feeds/packages/net/shadowsocks-libev /workdir/openwrt/package/lean/helloworld/
+    cp -r /workdir/lede/feeds/packages/net/shadowsocks-libev /workdir/openwrt/feeds/packages/net/shadowsocks-libev
+fi
+
+rm -rf /workdir/openwrt/feeds/packages/net/wget
+cp -rp /workdir/lede/package/lean/wget /workdir/openwrt/feeds/packages/net/wget
+cp -rp /workdir/lede/package/lean/wget /workdir/openwrt/package/lean/wget
+
+# Clone community packages to lean
+cd /workdir/openwrt/package/lean
+
+# Add Lienol's Packages
+#git clone --depth=1 https://github.com/Lienol/openwrt-package
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+
+# Add argon theme
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon ./luci-theme-argon/
+
+
+# Add openclash
+#cd /workdir/openwrt/package/community
+#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+#ARCH=mipsle-softfloat
+#cd /workdir/openwrt
+#mkdir -p files/etc/openclash/core
+#cd files/etc/openclash/core
+
+#clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+#clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+#clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+
+#wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
+#wget -qO- $clash_main_url | gunzip -c > clash
+#wget -qO- $clash_tun_url  | gunzip -c > clash_tun
+#wget -qO- $clash_game_url | tar xOvz > clash_game
+
+#chmod +x clash*
+
